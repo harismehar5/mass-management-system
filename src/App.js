c87ec50d6df0376c272f67406c745c522d6536d8
harismehar5 <harismehar3@gmail.com>
2022-11-23 20:08:57 +0500

    Login Screen

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from './layout/MainLayout';
 import {routes} from "./routes"
+import Login from './pages/login/Login';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/login' element={<Login />} />
         <Route path="/" element={<MainLayout />}>
           {routes}
         </Route>
